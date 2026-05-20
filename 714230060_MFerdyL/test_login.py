206501f014b31e11f2fac1a7a437f1ca74c64de3
ferdyleoza <ferdyleoza123@gmail.com>
2026-05-20 19:43:26 +0700

    1

---
+++ 714230060_MFerdyL/test_login.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+
+search = driver.find_element(By.NAME, "q")
+search.send_keys("Software Quality")
+
+print("Testing berhasil")
+
+driver.quit()
